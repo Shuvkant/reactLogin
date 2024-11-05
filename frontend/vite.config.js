333d752bf68dd3d9b69178afbfa575f4d60b63b4
Shuvkant <weird426344@gmail.com>
2024-11-05 19:22:26 +0545

    adding proxy server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        //we’re setting up a proxy for requests starting with /api.
+        //Any requests that match this pattern will be redirected to
+        //http:localhost:3000.
+      },
+    },
+  },
   plugins: [react()],
 });
