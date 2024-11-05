8100e2cce8773b1c1797c63083c882b037dd1727
Shuvkant <weird426344@gmail.com>
2024-11-05 15:41:16 +0545

    privateroute for unauthorized login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
-import Login from "./pages/Login.jsx";
-import "./App.css";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
+import { useState } from 'react'
+import Login from './pages/Login.jsx'
+import './App.css'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard.jsx'
+import PrivateRoute from './pages/PrivateRoute.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path='/' element={<Login />}></Route>
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
